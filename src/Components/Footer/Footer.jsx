49d6712fee716f36e137b6286a5007df5d8a359c
Ahmed Seleem <ahmedalaaseleem989@gmail.com>
2025-09-20 01:41:07 +0300

    first commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = () => {
       <p className="footer-copyright">
         &copy; 2025 | All rights reserved. Made by{" "}
         <a
-          href="https://ahmed12g4.github.io/My-Portfolio/"
+          href="https://portfolio-chi-nine-90.vercel.app/"
           target="_blank"
           rel="noreferrer"
         >
